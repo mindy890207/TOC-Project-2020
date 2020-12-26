d380582a30c567d6cd0aba6f02308576cac7d4ae
mindy890207 <mindy890207@gmail.com>
2020-12-26 19:45:22 +0800

    draw fsm picture

--- app.py
+++ app.py
@@ -111,7 +111,11 @@ def webhook_handler():
         print(f"REQUEST BODY: \n{body}")
         response = machine.advance(event)
         if response == False:
-            send_text_message(event.reply_token, "Not Entering any State")
+            if event.message.text == 'fsm':
+                send_text_message(event.reply_token,
+                                  'https://23d503f8989a.ngrok.io/show-fsm')
+            else:
+                send_text_message(event.reply_token, "Not Entering any State")
 
     return "OK"
 
