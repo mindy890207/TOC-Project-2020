a563a9ef2a5a7986da72a6a83e1d9ec020edb593
mindy890207 <mindy890207@gmail.com>
2020-12-26 18:30:24 +0800

    add state

--- app.py
+++ app.py
@@ -14,7 +14,7 @@ load_dotenv()
 
 
 machine = TocMachine(
-    states=["user", "state1", "state2"],
+    states=["user", "state1", "state2", "state3"],
     transitions=[
         {
             "trigger": "advance",
@@ -27,6 +27,12 @@ machine = TocMachine(
             "source": "user",
             "dest": "state2",
             "conditions": "is_going_to_state2",
+        },
+        {
+            "trigger": "advance",
+            "source": "user",
+            "dest": "state3",
+            "conditions": "is_going_to_state3",
         },
         {"trigger": "go_back", "source": ["state1", "state2"], "dest": "user"},
     ],
